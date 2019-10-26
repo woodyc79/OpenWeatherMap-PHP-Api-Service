4bc7c3c43e1daa917119fd8aa437e2d9b010fe18
Christian Holzmann <mail@innovations.at>
2019-10-26 23:37:50 +0200

    TYPO3 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.0',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '8.7.0-9.9.99',
 		),
 		'conflicts' => array(
 		),
